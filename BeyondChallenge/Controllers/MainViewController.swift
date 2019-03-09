be49cc60aa98973e90f73b0cd83c18c2c377268e
Günter <gunter.mhertz@gmail.com>
2019-03-09 15:37:43 -0300

    Fix view controller behavior

--- BeyondChallenge/Controllers/MainViewController.swift
+++ BeyondChallenge/Controllers/MainViewController.swift
@@ -14,13 +14,16 @@ class MainViewController: UIViewController {
     let disposedBag: DisposeBag = DisposeBag()
     
     override func loadView() {
-        let view = MainView(frame: UIScreen.main.bounds)
+        let view = MainView()
         
         //Need to make bind here before set FirstScreen as View or find a better way
         view.updateButton.rx.tap
-            .bind {
+            .subscribe(onNext: {
                 print("update button did tap do some action")
-            }
+                let dimmerVC = DimmerViewController()
+                dimmerVC.modalPresentationStyle = .overCurrentContext
+                self.present(dimmerVC, animated: true, completion: nil)
+            })
             .disposed(by: disposedBag)
         
         view.connectionStatus.rx
